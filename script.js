086e10329316047f1b0e0a557102d4376b5857eb
DeepakIbitwar <127574226+DeepakIbitwar@users.noreply.github.com>
2023-07-21 14:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const element = document.getElementById("colorSelect");
+
+function removeColor(){
+    element.remove(element.selectedIndex);
+}
+
+Volume of Sphere
